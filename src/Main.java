163c20569509f71897fb30ceab876c02d958f54f
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2022-12-03 15:42:52 +0900

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            if (i < 10) {
+                System.out.println("Hello World");
+            } else {
+                System.out.println("Hello");
+            }
+        }
+    }
+
 }
+
